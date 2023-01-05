3099f1311b87b83c0d38c86952ee473f896ca26d
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-01-05 15:37:59 +0330

    feat: add abstract declaration class

---
+++ src/assets/declaration/Declaration.java
@@ -0,0 +1,7 @@
+package assets.declaration;
+
+import assets.Tree;
+
+public abstract class Declaration extends Tree {
+}
+
---
+++ src/assets/declaration/VariableDeclaration.java
@@ -0,0 +1,5 @@
+package assets.declaration;
+
+public abstract class VariableDeclaration extends Declaration {
+
+}
