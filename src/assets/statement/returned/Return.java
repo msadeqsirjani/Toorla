a2dad6f550974c93cfb02c6ff636c9e9a0cdb971
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-01-05 16:53:00 +0330

    feat: add return expression

---
+++ src/assets/statement/returned/Return.java
@@ -0,0 +1,27 @@
+package assets.statement.returned;
+
+import assets.expression.Expression;
+import assets.statement.Statement;
+import visitor.Visitor;
+
+public class Return extends Statement {
+    private Expression returned;
+
+    public Return(Expression returned) {
+        this.returned = returned;
+    }
+
+    public Expression getReturned() {
+        return returned;
+    }
+
+    @Override
+    public <T> T visit(Visitor<T> visitor) {
+        return visitor.visit(this);
+    }
+
+    @Override
+    public String toString() {
+        return "return";
+    }
+}
